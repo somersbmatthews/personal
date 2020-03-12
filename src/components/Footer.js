3e05b3b254393fd33cdf233bedde8d5a034b97e4
Somers Matthews <somersbmatthews@gmail.com>
2020-03-11 21:29:18 -0500

    added code to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,14 @@
 import React from 'react'
 
 import StatelessComponent from '../hoc/StatelessComponent'
+
+import classes from "./Footer.css"
+
+const Footer = () => {
+
+
+
+
+}
+
+export default Footer;
